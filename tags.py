341fff3f8d146e231d47ec55d35f7c10c580b29d
thebarbershopper <cld251@gmail.com>
2017-09-01 16:51:09 -0500

    Basic tags.select() implemented

--- tags.py
+++ tags.py
@@ -7,10 +7,26 @@ class TagsDatabase(object):
     """
     Database containing the dictionary of tags in the current database.
 
-    TagsDatabase must also hold the BinaryView in order to query/add comments and query function information.
+    ``TagsDatabase`` must also hold the ``BinaryView`` in order to query/add comments and query function information.
+
+    Dict Structure::
+
+        {
+            0xdeadbeef: {
+                'synopsis': 'Calls vulnerable function',
+                'calls': ['gets']
+            },
+            0xcafebabe: {
+                'synopsis': 'Wrapper around function',
+                'break': '.printf "Useful wrapper"'
+            },
+            'synopsis': [0xdeadbeef, 0xdeadbeef],
+            'calls': [0xdeadbeef],
+            'break': [0xcafebabe],
+        }
     """
     def __init__(self, bv):
-        self._db = defaultdict(dict)
+        self._db = {}
         self.bv = bv
 
     def add(self, address, tagname, data):
@@ -23,6 +39,9 @@ class TagsDatabase(object):
         :param str tagname: Name of the tag being applied
         :param str data: Tag data 
         :return: None
+        :Example:
+            
+	    >>> tags.add(here, 'note', 'testing tags')
         """
         curr_func = self.bv.get_functions_containing(address)
         if curr_func == None:
@@ -30,7 +49,15 @@ class TagsDatabase(object):
             return
 
         curr_func = curr_func[0]
+        if self._db.get(address) == None:
+                self._db[address] = {}
+
+        if self._db.get(tagname) == None:
+                self._db[tagname] = []
+
         self._db[address][tagname] = data
+        self._db[tagname].append(address)
+
         curr_comment = curr_func.get_comment_at(address)
         new_tag = self._create_tag(tagname, data)
         if new_tag not in curr_comment:
@@ -62,7 +89,57 @@ class TagsDatabase(object):
         """
         return '[{}] {}\n'.format(tagname, data)
 
-    """
-    def select(tagname):
-        '''Return all tags with the given tagname'''
-    """
+    def select(self, *args):
+        """
+        Return all tags with the given tagname
+
+        :param str/int/long addr/tagname: Address or Tagname
+        :param str/int/long addr/tagname: Address or Tagname
+        :return: Requested tags or addresses
+        :rtype: dict/list/str
+        :Example:
+
+	    >>> tags.add(here, 'note', 'testing tags')
+
+	    >>> tags.select(here)
+	    {'note': 'testing tags'}
+
+	    >>> tags.select('note')
+	    [134515550L]
+
+	    >>> tags.select(here, 'note')
+	    'testing tags'
+
+	    >>> tags.select('note', here)
+	    'testing tags'
+        """
+        if len(args) > 2:
+            print("tags.select only takes up to 2 arguments")
+            return
+
+        elif len(args) == 1: 
+            arg = args[0]
+            if not isinstance(arg, (long, int, str)):
+                print("tags.select can only take long/ints for addresses and str for tagnames. {} not available".format(type(arg)))
+                return
+            if isinstance(arg, (int, long)):
+                return self._db[arg]
+            elif isinstance(args[0], str):
+                return self._db.get(arg, 'Tag {} not found'.format(arg))
+
+        elif len(args) == 2:
+            for arg in args:
+                if not isinstance(arg, (long, int, str)):
+                    print("tags.select can only take long/ints for addresses and str for tagnames. {} not available".format(type(arg)))
+                    return
+            if type(arg[0]) == type(arg[1]):
+                print("tags.select argument are mutually exclusive str and long/int")
+                return
+            if isinstance(args[0], (long, int)) and isinstance(args[1], str):
+                addr, tagname = args
+                return self._db[addr][tagname]
+            elif isinstance(args[0], (str)) and isinstance(args[1], (long, int)):
+                tagname, addr = args
+                return self._db[addr][tagname]
+            else:
+                print("Not sure what was given to tags.select.. shouldn't get here")
