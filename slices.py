8b36a5838ff7c15b1b8a660d096dfdff35525a4c
thebarbershopper <cld251@gmail.com>
2017-10-28 14:27:01 -0500

    Backward working through functions

--- slices.py
+++ slices.py
@@ -1,6 +1,7 @@
 import binaryninja
 from binaryninja import *
 from binaryninja.mediumlevelil import SSAVariable, MediumLevelILFunction
+from Queue import Queue
 
 def forward(instruction):
     """
@@ -59,7 +60,16 @@ def forward(instruction):
         return []
 
 def backward(instruction):
-    # switch to SSA form (this does nothing if it's already SSA).
+    """
+    Assumes the instruction is a variable
+
+    Example usage:
+
+    for a in slices.backward(mlilssa[51].params[0]):
+	print('-' * 40)
+	for il in reversed(a):
+	    print(il.function.source_function.name, il)
+    """
 
     function = instruction.function
     if not isinstance(function, MediumLevelILFunction):
@@ -67,39 +77,56 @@ def backward(instruction):
     else:
         ssa = function.ssa_form
 
-    if not isinstance(instruction.ssa_form.operands[0], SSAVariable):
-        return set()
 
-    function = instruction.function
-    instruction_queue = set([instruction.ssa_form.instr_index])
-    visited_instructions = set()
+    next_instr = ssa[ssa.get_ssa_var_definition(instruction.src)]
+
+    instruction_queue = Queue()
+    # instruction_queue.put([instruction.ssa_form])
+    instruction_queue.put([instruction, next_instr])
 
     variables = set()
 
-    while instruction_queue:
-        visit_index = instruction_queue.pop()
+    results = []
 
-        if visit_index is None or visit_index in visited_instructions:
-            continue
-
-        instruction_to_visit = function[visit_index]
+    while instruction_queue.qsize() > 0:
+        curr_path = instruction_queue.get()
+        visit_index = curr_path[-1].instr_index
+        instruction_to_visit = ssa[visit_index]
 
         if instruction_to_visit is None:
             continue
 
+        if not instruction_to_visit.vars_read:
+            results.append(curr_path)
+            continue
+
         for new_var in instruction_to_visit.vars_read:
-            instruction_queue.add(
-                function.get_ssa_var_definition(
-                    new_var
-                )
-            )
+            if not isinstance(new_var, mediumlevelil.SSAVariable):
+                results.append(curr_path)
+                continue
 
-        variables.update(
-            [(var.var.identifier, var.version)
-                for var in instruction_to_visit.vars_read]
-        )
+            next_var_index = ssa.get_ssa_var_definition(new_var)
+            if next_var_index == None:
+                """
+                (Pdb) curr_path[-1]
+                <il: ecx#1 = arg3#0>
+                (Pdb) curr_path[-1].src.src.var
+                <var void* arg3>
+                """
+                param_num = list(ssa.source_function.parameter_vars).index(curr_path[-1].src.src.var)
+                bv = ssa.source_function.view
+                xrefs = bv.get_code_refs(ssa.source_function.start)
+                for xref in xrefs:
+                    xref_il = xref.function.get_low_level_il_at(xref.address).medium_level_il.ssa_form
+                    xref_var = xref_il.params[param_num]
+                    back_results = backward(xref_var)
+                    for path in back_results:
+                        final_path = curr_path[:]
+                        final_path.extend(path)
+                        results.append(final_path)
+            else:
+                new_path = curr_path[:]
+                new_path.append(ssa[next_var_index]) 
+                instruction_queue.put(new_path)
 
-        visited_instructions.add(visit_index)
-
-    return visited_instructions
-
+    return results
